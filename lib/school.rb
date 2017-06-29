6db6b875ac78d181c89e1ecd5c0f52279703698d
RomanJohnson <roman.johnson+git@gmail.com>
2017-06-29 13:37:33 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+
+require 'pry'
+class School
+
+attr_accessor :roster, :name, :grade
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+
+    def add_student(name, grade)
+      @roster[grade] ||= []
+      @roster[grade] << name
+    end
+
+    def grade(grade)
+      @roster[grade]
+    end
+
+      def sort
+        Hash[@roster.sort.map { |a,b| [a, b.sort] }]
+      #  @roster.each do |key, value|
+
+        # @roster[key].sort
+        # end
+        # @roster.sort_by {|key, value| key}.to_h
+        # @roster.sort_by {|key, value| value}.to_h
+    end
+end
